3061711c40439b11d4d6da185bee506ddb39ac35
Ayodele Samuel Atoyebi ASA <105869141+sirsamuelayodele@users.noreply.github.com>
2024-10-07 08:11:17 +0100

    Update high_profile.py

--- high_profile.py
+++ high_profile.py
@@ -1,122 +1,164 @@
-import pandas as pd
 import random
-from faker import Faker
+import faker
+import pandas as pd
 
-fake = Faker('en_NG')  # Nigeria-specific locale
-Faker.seed(42)
+# Initialize Faker
+fake = faker.Faker('en_NG')  # Nigerian locale for realistic context
 
-# 150 Yoruba first and last names (sample from earlier code)
-sample_first_names = [
-    'Adebayo', 'Ayotunde', 'Folake', 'Oluwatobi', 'Bamidele', 'Kehinde', 'Temidayo', 'Iretiola', 
-    'Funke', 'Gbenga', 'Adedayo', 'Olufemi', 'Oluwaseun', 'Bola', 'Yetunde', 'Damilola', 
-    'Oluwakemi', 'Sola', 'Omotola', 'Olajide', 'Adeniyi', 'Olumide', 'Boluwatife', 'Adeola', 
-    'Titilayo', 'Oluwadamilare', 'Adekunle', 'Abimbola', 'Ayodele', 'Segun', 'Tayo', 
-    'Olawale', 'Mobolaji', 'Omowunmi', 'Oluwanifemi', 'Fiyinfoluwa', 'Ajibola', 'Tolulope', 
-    'Adetola', 'Opeoluwa', 'Adenike', 'Ayomide', 'Babajide', 'Morenike', 'Olujimi', 'Kikelomo', 
-    'Opeyemi', 'Adeyinka', 'Anuoluwapo', 'Oluwatoyin', 'Tunde', 'Oluwole', 'Ibitoye', 'Gbolahan', 
-    'Demilade', 'Oluwadamilola', 'Babatunde', 'Tiwalade', 'Erioluwa', 'Mayowa', 'Oluwanifemi', 
-    'Olushola', 'Ademola', 'Oreoluwa', 'Oluwapelumi', 'Ifeoluwa', 'Olamide', 'Olayinka', 
-    'Oluwakayode', 'Taiwo', 'Tope', 'Oluwatobi', 'Funmilayo', 'Oluwole', 'Adebimpe', 'Tobiloba', 
-    'Yewande', 'Afolabi', 'Adunni', 'Oluwafemi', 'Adetayo', 'Adeyemi', 'Adesola', 'Oluwakemi', 
-    'Oreofe', 'Oluwadare', 'Ifedayo', 'Ajoke', 'Abiodun', 'Olatunde', 'Oluwashina', 'Oluwadamilola',
-    'Aderonke', 'Oluwadara', 'Oluwakunle', 'Titilope', 'Folusho', 'Adejoke', 'Aderemi', 'Oluwasola', 
-    'Mofoluwake', 'Oluwasemilore', 'Olufolake', 'Olamiposi', 'Oluwatimilehin', 'Adunola', 
-    'Olufisayo', 'Adeola', 'Oluwatobi', 'Oluwadamilola', 'Oladimeji', 'Adenike', 'Adeniran', 
-    'Olumuyiwa', 'Iretiola', 'Boluwaji', 'Oluwadunsin', 'Oluwasegun', 'Oluwakorede', 'Oladayo', 
-    'Mofeoluwa', 'Oluwatosin', 'Oluwole', 'Akinola', 'Oluwadamilare', 'Adeniran', 'Akinbode',
-    'Opeyemi', 'Oluwafunmilayo', 'Olubunmi', 'Folarin', 'Tosin', 'Adeniran', 'Toluwalase', 
-    'Olatunde', 'Oluwanishola'
+# Define 150 unique Yoruba first names and 150 unique Yoruba last names
+yoruba_first_names = [
+    'Adebayo', 'Adetokunbo', 'Adeola', 'Adeshola', 'Adetunji', 'Ademola', 
+    'Folake', 'Olufunke', 'Tunde', 'Kehinde', 'Taiwo', 'Yejide', 
+    'Femi', 'Oluwaseun', 'Olamide', 'Temitope', 'Bukola', 'Damilola', 
+    'Ayotunde', 'Ireoluwa', 'Tosin', 'Seun', 'Dayo', 'Modupe', 
+    'Opeyemi', 'Olawale', 'Chinonso', 'Chidera', 'Emeka', 'Obinna', 
+    'Kenechukwu', 'Ifedayo', 'Tobiloba', 'Oyinlola', 'Tolu', 'Ojo', 
+    'Folorunsho', 'Olatunde', 'Olatunji', 'Oluwatobi', 'Oluwatimilehin', 
+    'Oluwanifemi', 'Ayo', 'Yinka', 'Temiloluwa', 'Akin', 'Mide', 
+    'Yemi', 'Seyi', 'Dayo', 'Kiki', 'Fiyinfoluwa', 'Damilare', 
+    'Yejide', 'Titilayo', 'Jibola', 'Oluwakemi', 'Olajumoke', 
+    'Abiola', 'Olasunkanmi', 'Adeyemi', 'Temitope', 'Oluwanisola', 
+    'Oluwasemilore', 'Funmilola', 'Eniola', 'Moyosore', 'Toyin', 
+    'Adewale', 'Abike', 'Anu', 'Doyin', 'Omolara', 'Ayanfe', 
+    'Olamijuwon', 'Folarin', 'Oluwatofunmi', 'Adunola', 'Ireti', 
+    'Afolabi', 'Temilade', 'Yetunde', 'Oreoluwa', 'Ayoade', 
+    'Bimpe', 'Ibukun', 'Kehinde', 'Oluwatobiloba', 'Amarachi', 
+    'Adebisi', 'Oni', 'Ojo', 'Ayanfe', 'Afolabi', 'Boluwatife', 
+    'Simi', 'Abike', 'Tawakalitu', 'Temitope', 'Dapo', 'Modupe', 
+    'Afishetan', 'Oluwaseun', 'Fatimah', 'Deola', 'Bola', 
+    'Titi', 'Oluwaseun', 'Lanre', 'Bola', 'Fadekemi', 
+    'Micheal', 'Samuel', 'Ikechukwu', 'Chike', 'Chukwudi', 
+    'Chijioke', 'Micheal', 'Philip', 'Abubakar', 'Ali', 
+    'Usman', 'Sadiq', 'Musa', 'Zainab', 'Aisha', 
+    'Fatima', 'Hauwa', 'Bashir', 'Halima', 'Kabir', 
+    'Zahra', 'Yusuf', 'Olayemi', 'Ibraheem', 'Khadijat'
 ]
 
-sample_last_names = [
-    'Olawale', 'Akinyemi', 'Fasola', 'Adesanya', 'Adebisi', 'Ogunleye', 'Akinsola', 'Fagbemi', 
-    'Soyinka', 'Oyelowo', 'Adebanjo', 'Adefemi', 'Ajayi', 'Awolowo', 'Oshodi', 'Babalola', 'Oniru', 
-    'Ogundele', 'Odetola', 'Akinlade', 'Egunjobi', 'Osunbade', 'Salako', 'Ibrahim', 'Adedeji', 
-    'Akinola', 'Olatunji', 'Olubode', 'Olowookere', 'Oladokun', 'Aderibigbe', 'Omodara', 'Salami', 
-    'Fadeyi', 'Ajagbe', 'Aderemi', 'Ogunlana', 'Olatoye', 'Ogbemudia', 'Adepoju', 'Ogunbiyi', 
-    'Alade', 'Olokun', 'Adegunle', 'Olanipekun', 'Adetunji', 'Adekanbi', 'Balogun', 'Obafemi', 
-    'Ajiboye', 'Adelabu', 'Adeniran', 'Olumide', 'Adediran', 'Akinyemi', 'Oyekanmi', 'Odugbemi', 
-    'Adebayo', 'Olajide', 'Oladimeji', 'Olagunju', 'Ogunsanya', 'Okanlawon', 'Olaniyan', 
-    'Oladimeji', 'Ogundipe', 'Onasanya', 'Adeboye', 'Akinyemi', 'Odusanya', 'Onibudo', 'Arowolo', 
-    'Olanipekun', 'Adebiyi', 'Olusola', 'Ogunfemi', 'Aina', 'Adeola', 'Olufemi', 'Ajibola', 'Akingbade', 
-    'Olubode', 'Osunwole', 'Olubodun', 'Olufon', 'Adeola', 'Ajayi', 'Ogunwale', 'Olaoluwa', 
-    'Adeoye', 'Akinfemi', 'Akintunde', 'Akinbode', 'Ajayi', 'Adeyemo', 'Akintola', 'Alimi', 
-    'Akanni', 'Adebajo', 'Awolola', 'Akinlotan', 'Oladele', 'Adebayo', 'Ogunbi', 'Olugbemi', 
-    'Olufon', 'Olumide', 'Adebiyi', 'Ogunrinade', 'Olufon', 'Adedokun', 'Olusola', 'Adeyeye', 
-    'Olufunke', 'Oyekunle', 'Odukoya', 'Adewunmi', 'Onajobi', 'Ogundipe', 'Olatunbosun', 
-    'Onabanjo', 'Osinbajo', 'Adebimpe', 'Adedeji', 'Ogundipe', 'Akinbobola', 'Ajiboye', 
-    'Adesuyi', 'Akinfenwa', 'Oduwole', 'Ajibola', 'Oluwabiyi', 'Adewusi', 'Adebajo'
+yoruba_last_names = [
+    'Adebola', 'Adeyemi', 'Adelani', 'Adedoyin', 'Afolabi', 'Alabi', 
+    'Bello', 'Dada', 'Emmanuel', 'Ogunleye', 'Okwuosa', 'Akanbi', 
+    'Oba', 'Olajide', 'Olaoye', 'Olasunkanmi', 'Olujimi', 
+    'Olofinjana', 'Oluwatoyin', 'Olatunji', 'Okoro', 'Okunola', 
+    'Okwuosa', 'Olayiwola', 'Oni', 'Salami', 'Saliu', 
+    'Shodipo', 'Sulaimon', 'Tajudeen', 'Umar', 'Yusuf', 
+    'Zubair', 'Omoniyi', 'Alabi', 'Adedayo', 'Adelakun', 
+    'Abdul', 'Abubakar', 'Abiola', 'Alabi', 'Adetunji', 
+    'Ayobami', 'Emeka', 'Ibrahim', 'Izuchukwu', 'Nwachukwu', 
+    'Obinna', 'Okwuosa', 'Okwudili', 'Onyeabor', 'Onyemaechi', 
+    'Tijani', 'Wale', 'Yekini', 'Abidemi', 'Adeagbo', 
+    'Adelani', 'Ajayi', 'Akinyemi', 'Ali', 'Amadi', 
+    'Azeez', 'Chukwuma', 'Chinyere', 'Chinonso', 'Chuka', 
+    'Daniel', 'Ehi', 'Emeka', 'Fadipe', 'Fola', 
+    'Gideon', 'Ibe', 'Igbinosa', 'Innocent', 'Iru', 
+    'Joshua', 'Kehinde', 'Moses', 'Nnaji', 'Nwanne', 
+    'Obinna', 'Oghenero', 'Okwudili', 'Oluoch', 'Olayiwola', 
+    'Oni', 'Osawe', 'Ozuru', 'Ozuru', 'Suleiman', 
+    'Tade', 'Tajudeen', 'Temitope', 'Wasiu', 'Yekeen'
 ]
 
-sample_states = ['Lagos', 'Oyo', 'Ogun', 'Osun']
+# Define weighted random selection function
+def weighted_choice(options, weights):
+    return random.choices(options, weights)[0]
 
-# Function to generate student data
-def generate_student_data(num_records=400):
-    student_data = []
-    for i in range(1, num_records + 1):
-        first_name = random.choice(sample_first_names)
-        last_name = random.choice(sample_last_names)
-        state = random.choice(sample_states)
-        age = random.randint(16, 21)
-        gender = random.choice(['Male', 'Female'])
-        student_data.append([i, first_name, last_name, age, gender, state])
-    return student_data
+# Generate student name from Yoruba names only
+def generate_student_name():
+    first_name = random.choice(yoruba_first_names)
+    last_name = random.choice(yoruba_last_names)
+    return first_name, last_name
 
-# Function to generate parent data
-def generate_parent_data(student_data):
-    parent_data = []
-    for student in student_data:
-        parent_income = random.randint(500000, 2000000)
-        parent_education_level = random.choice(['High School', 'Bachelor\'s Degree', 'Master\'s Degree', 'PhD'])
-        parent_data.append([student[0], fake.name(), parent_income, parent_education_level])
-    return parent_data
+# Generate age based on student class
+def generate_age(student_class):
+    if student_class == 'SS1':
+        return random.randint(13, 14)
+    elif student_class == 'SS2':
+        return random.randint(14, 16)
+    else:
+        return random.randint(15, 18)
 
-# Function to generate library usage data
-def generate_library_usage(student_data):
-    library_usage = []
-    for student in student_data:
-        hours_spent = random.uniform(5, 8)
-        books_read = random.randint(6, 9)
-        library_usage.append([student[0], round(hours_spent, 2), books_read])
-    return library_usage
+# Create empty lists to hold the records for each table
+students = []
+parents = []
+teachers = []
+school_facilities = []
+performances = []
 
-# Function to generate extra-curricular activity data
-def generate_extra_curricular(student_data):
-    extra_curricular = []
-    activities = ['Sport', 'Drama', 'Music', 'Debate']
-    for student in student_data:
-        activity = random.choice(activities)
-        extra_curricular.append([student[0], activity])
-    return extra_curricular
+# Generate data for 300 students
+for student_id in range(1, 301):
+    # Generate student name and attributes
+    first_name, last_name = generate_student_name()
+    student_class = random.choice(['SS1', 'SS2', 'SS3'])  # Class selection
+    age = generate_age(student_class)
+    gender = random.choice(['Male', 'Female'])
+    absent_times = weighted_choice([0, 1, 2, 3, 4, 5], [0.45, 0.45, 0.10, 0.01, 0.01, 0.01])  # 90% between 0 and 2
+    address_type = random.choice(['Rural', 'Urban'])
+    goes_out_with_friends = random.choice(['Yes', 'No'])
+    desire_higher_education = 'Yes'  # 100% Yes
+    family_support = weighted_choice(range(4, 6), [0.05, 0.95])  # 95% at 5
+    internet_access = 'Yes'  # 100% Yes
+    commute_time = weighted_choice(range(5, 21), [0.8] * 6 + [0.2] * 16)  # 80% between 5 and 10 mins
+    living_status = weighted_choice(['T', 'A'], [0.33, 0.67])  # 67% A
 
-# Function to generate performance data
-def generate_performance_data(student_data):
-    performance_data = []
-    for student in student_data:
-        exam_type = 'JAMB'
-        score = random.choices([random.randint(300, 335), random.randint(245, 299)], weights=[5, 95])[0]
-        performance_data.append([student[0], exam_type, score])
-    return performance_data
+    students.append({
+        'student_id': student_id,
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+        'gender': gender,
+        'student_class': student_class,
+        'absent_times': absent_times,
+        'address_type': address_type,
+        'goes_out_with_friends': goes_out_with_friends,
+        'desire_higher_education': desire_higher_education,
+        'family_support': family_support,
+        'internet_access': internet_access,
+        'commute_time': commute_time,
+        'living_status': living_status
+    })
 
-# Generate data
-students = generate_student_data()
-parents = generate_parent_data(students)
-library_usage = generate_library_usage(students)
-extra_curricular = generate_extra_curricular(students)
-performance = generate_performance_data(students)
+    # Generate parent attributes
+    parent_income = weighted_choice(range(200000, 600001), [0.03] * 200001 + [0.97] * 400001)  # 97% between 400k and 600k
+    parents.append({
+        'parent_id': student_id,  # Use student_id as parent_id for simplicity
+        'parent_income': parent_income,
+        'mother_job': fake.job(),
+        'father_job': fake.job(),
+    })
 
-# Convert to DataFrames
-students_df = pd.DataFrame(students, columns=['Student ID', 'First Name', 'Last Name', 'Age', 'Gender', 'State'])
-parents_df = pd.DataFrame(parents, columns=['Student ID', 'Parent Name', 'Parent Income', 'Parent Education Level'])
-library_usage_df = pd.DataFrame(library_usage, columns=['Student ID', 'Hours Spent in Library', 'Books Read'])
-extra_curricular_df = pd.DataFrame(extra_curricular, columns=['Student ID', 'Extra Curricular Activity'])
-performance_df = pd.DataFrame(performance, columns=['Student ID', 'Exam Type', 'Score'])
+    # Generate teacher attributes
+    teachers.append({
+        'teacher_id': student_id,  # Use student_id as teacher_id for simplicity
+        'qualification': random.choice(['NCE', 'B.Ed', 'M.Ed', 'PhD']),
+        'experience_years': random.randint(1, 20),
+        'subject_specialization': random.choice(['Mathematics', 'Science', 'Literature', 'History'])
+    })
 
-# Save to CSV
-students_df.to_csv('students_data.csv', index=False)
-parents_df.to_csv('parents_data.csv', index=False)
-library_usage_df.to_csv('library_usage.csv', index=False)
-extra_curricular_df.to_csv('extra_curricular.csv', index=False)
-performance_df.to_csv('performance_data.csv', index=False)
+    # Generate school facilities
+    school_facilities.append({
+        'facility_id': student_id,  # Use student_id as facility_id for simplicity
+        'facility_name': random.choice(['Library', 'Science Lab', 'Sports Ground', 'Computer Lab']),
+        'availability': random.choice(['Yes', 'No']),
+        'condition': random.choice(['Good', 'Average', 'Poor'])
+    })
 
-print("Data generation completed and saved as CSV files.")
+    # Generate performance data
+    performance_score = weighted_choice(range(125, 224), [0.01] * 100 + [0.99] * 99)  # 1% between 195 and 223
+    performances.append({
+        'performance_id': student_id,  # Use student_id as performance_id for simplicity
+        'exam_score': performance_score,
+    })
+
+# Create DataFrames for each entity
+students_df = pd.DataFrame(students)
+parents_df = pd.DataFrame(parents)
+teachers_df = pd.DataFrame(teachers)
+school_facilities_df = pd.DataFrame(school_facilities)
+performances_df = pd.DataFrame(performances)
+
+# Save DataFrames to CSV
+students_df.to_csv('students.csv', index=False)
+parents_df.to_csv('parents.csv', index=False)
+teachers_df.to_csv('teachers.csv', index=False)
+school_facilities_df.to_csv('school_facilities.csv', index=False)
+performances_df.to_csv('performances.csv', index=False)
+
+print("CSV files generated successfully!")
